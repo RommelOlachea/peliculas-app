98350c9a0e21aa5bcd54d327759de5e631487e92
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-10-25 14:58:22 -0600

    Poster y titulo de la peliculaP

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,7 +11,10 @@ class DetailScreen extends StatelessWidget {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
+      slivers: [
+        _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([_PosterAndTtile()])),
+      ],
     ));
   }
 }
@@ -44,6 +47,66 @@ class _CustomAppBar extends StatelessWidget {
             placeholder: AssetImage('assets/loading.gif'),
             image: NetworkImage('https://via.placeholder.com/500x300'),
             fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class _PosterAndTtile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('http://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.Title',
+                style: Theme.of(context).textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
