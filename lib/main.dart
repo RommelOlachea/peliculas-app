d5f69fbdaa3dc3fc23d1669d503e99210ff78c69
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-10-27 14:47:08 -0600

    Provider y paquete HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/screens.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
