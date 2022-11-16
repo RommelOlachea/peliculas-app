2f0b942a66256ddf7074d09a525b66c6e845f55d
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-11-16 14:56:52 -0700

    Creacion del delegate

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -6,14 +6,21 @@ class MovieSearchDelegate extends SearchDelegate {
   String? get searchFieldLabel => 'Buscar peliculas';
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    return [const Text('buildActions')];
+    // Creamos un boton para limpiar el campo de busqueda
+    return [
+      IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    return const Text('buildLeading');
+    // Regremos a la pantalla principal, en close, ponemos lo que queremos regresar,
+    //puede ser cualquier tipo de dato, en este caso null
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
   }
 
   @override
@@ -24,7 +31,18 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    return Text('buildSugestions: $query');
+    // cuerpo de la busqueda, donde mostramos los resultados
+    if (query.isEmpty) {
+      return Container(
+        child: Center(
+            child: Icon(
+          Icons.movie_creation_outlined,
+          color: Colors.black38,
+          size: 130,
+        )),
+      );
+    }
+
+    return Container();
   }
 }
